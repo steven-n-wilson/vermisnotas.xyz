187438cc5ed96e0de84cd59b3beac908920ba105
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2020-09-12 16:23:20 -0600

    integrates scraper, google sheets and selenium

--- website_login/WebsitesLoginAutomation.py
+++ website_login/WebsitesLoginAutomation.py
@@ -1,14 +1,10 @@
-from selenium import webdriver
 import yaml
 import time
-
-conf = yaml.load(open('loginDetails.yml'),
-                 Loader=yaml.FullLoader)
-myWA_email = conf['webAssign_user']['email']
-myWA_password = conf['webAssign_user']['cengagePassword']
-
-
-driver = webdriver.Chrome()
+import gspread
+import requests
+import datetime
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
 def login(url, usernameId, username, passwordId, password, submit_buttonId):
@@ -18,8 +14,36 @@ def login(url, usernameId, username, passwordId, password, submit_buttonId):
     driver.find_element_by_name(submit_buttonId).click()
 
 
+def request(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    return soup
+
+
+def parse(soup):
+    pass
+
+
+def output(notas):
+    gc = gspread.service_account(filename='creds.json')
+    sh = gc.open('scrapetosheets').sheet1
+
+    sh.append_row(['first', 'second', 'third'])
+
+
+conf = yaml.load(open('loginDetails.yml'),
+                 Loader=yaml.FullLoader)
+myWA_email = conf['webAssign_user']['email']
+myWA_password = conf['webAssign_user']['cengagePassword']
+
+
+driver = webdriver.Chrome()
+
 login('https://www.webassign.net/wa-auth/login',
       'email', myWA_email, 'cengagePassword', myWA_password, 'Login')
 
 time.sleep(10)
-driver.find_element_by_partial_link_text('Grades').click()
+# driver.find_element_by_partial_link_text('Grades').click()
+current_url = driver.current_url
+
+print(request(current_url))
