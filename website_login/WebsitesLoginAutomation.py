20f0c181770398c0c71e6ed42ebd3da5577feb31
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2020-09-12 14:32:33 -0600

    automated login and grades click

--- website_login/WebsitesLoginAutomation.py
+++ website_login/WebsitesLoginAutomation.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 import yaml
+import time
 
-conf = yaml.load(open('website_login\loginDetails.yml'),
+conf = yaml.load(open('loginDetails.yml'),
                  Loader=yaml.FullLoader)
 myWA_email = conf['webAssign_user']['email']
 myWA_password = conf['webAssign_user']['cengagePassword']
@@ -14,9 +15,11 @@ def login(url, usernameId, username, passwordId, password, submit_buttonId):
     driver.get(url)
     driver.find_element_by_id(usernameId).send_keys(username)
     driver.find_element_by_id(passwordId).send_keys(password)
-    # driver.find_element_by_id(submit_buttonId).click()
     driver.find_element_by_name(submit_buttonId).click()
 
 
 login('https://www.webassign.net/wa-auth/login',
       'email', myWA_email, 'cengagePassword', myWA_password, 'Login')
+
+time.sleep(10)
+driver.find_element_by_partial_link_text('Grades').click()
