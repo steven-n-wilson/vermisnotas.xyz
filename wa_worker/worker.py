57ded5d0dd8db3148bdefdcd0e455bbd35bd6bd5
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2020-09-26 15:04:34 -0600

    changed file route and navigates to class scores

--- wa_worker/worker.py
+++ wa_worker/worker.py
@@ -5,6 +5,7 @@ import requests
 import datetime
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 
 
 def login(url, usernameId, username, passwordId, password, submit_buttonId):
@@ -25,23 +26,28 @@ def parse(soup):
 
 
 def output(notas):
-    gc = gspread.service_account(filename='creds.json')
+    gc = gspread.service_account(filename='wa_worker/creds.json')
     sh = gc.open('scrapetosheets').sheet1
 
     sh.update('A1', str(notas))
 
 
-conf = yaml.load(open('loginDetails.yml'), Loader=yaml.FullLoader)
+conf = yaml.load(open('wa_worker/loginDetails.yml'), Loader=yaml.FullLoader)
 myWA_email = conf['webAssign_user']['email']
 myWA_password = conf['webAssign_user']['cengagePassword']
 
-
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path='wa_worker/chromedriver.exe')
 login('https://www.webassign.net/wa-auth/login', 'email',
       myWA_email, 'cengagePassword', myWA_password, 'Login')
 
 time.sleep(10)
-# driver.find_element_by_partial_link_text('Grades').click()
+driver.find_element_by_xpath('//*[@id="list"]/li[5]/a').click()
+driver.find_element_by_xpath('//*[@id="list"]/li[5]/ul/li[3]/a').click()
+# driver.find_element_by_xpath('//*[@id="scourse"]').click()
+select = Select(driver.find_element_by_id('scourse'))
+select.select_by_visible_text("MC 106, section A, Fall 2020")
+
+
 current_url = driver.current_url
 
 soup = request(current_url)
