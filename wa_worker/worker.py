e95527f13de83a41dcc0ab01615d2168176b6b44
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2020-09-26 18:00:28 -0600

    modified login func and created go to class func

--- wa_worker/worker.py
+++ wa_worker/worker.py
@@ -1,18 +1,17 @@
-import yaml
-import time
-import gspread
-import requests
-import datetime
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
+
+def login(username, password):
+    driver.get('https://www.webassign.net/wa-auth/login')
+    driver.find_element_by_id('email').send_keys(username)
+    driver.find_element_by_id('cengagePassword').send_keys(password)
+    driver.find_element_by_name('Login').click()
+    time.sleep(10)
 
 
-def login(url, usernameId, username, passwordId, password, submit_buttonId):
-    driver.get(url)
-    driver.find_element_by_id(usernameId).send_keys(username)
-    driver.find_element_by_id(passwordId).send_keys(password)
-    driver.find_element_by_name(submit_buttonId).click()
+def go_to_class(class_name):
+    driver.find_element_by_xpath('//*[@id="list"]/li[5]/a').click()
+    driver.find_element_by_xpath('//*[@id="list"]/li[5]/ul/li[3]/a').click()
+    select = Select(driver.find_element_by_id('scourse'))
+    select.select_by_visible_text(class_name)
 
 
 def request(url):
@@ -32,23 +31,26 @@ def output(notas):
     sh.update('A1', str(notas))
 
 
-conf = yaml.load(open('wa_worker/loginDetails.yml'), Loader=yaml.FullLoader)
-myWA_email = conf['webAssign_user']['email']
-myWA_password = conf['webAssign_user']['cengagePassword']
+if __name__ == "__main__":
 
-driver = webdriver.Chrome(executable_path='wa_worker/chromedriver.exe')
-login('https://www.webassign.net/wa-auth/login', 'email',
-      myWA_email, 'cengagePassword', myWA_password, 'Login')
+    import yaml
+    import time
+    import gspread
+    import requests
+    import datetime
+    from bs4 import BeautifulSoup
+    from selenium import webdriver
+    from selenium.webdriver.support.ui import Select
 
-time.sleep(10)
-driver.find_element_by_xpath('//*[@id="list"]/li[5]/a').click()
-driver.find_element_by_xpath('//*[@id="list"]/li[5]/ul/li[3]/a').click()
-# driver.find_element_by_xpath('//*[@id="scourse"]').click()
-select = Select(driver.find_element_by_id('scourse'))
-select.select_by_visible_text("MC 106, section A, Fall 2020")
+    driver = webdriver.Chrome(executable_path='wa_worker/chromedriver.exe')
 
+    WA_email = 'stevenwilsonnunez@ufm.edu'
+    WA_password = 'Kelpforest13?'
 
-current_url = driver.current_url
+    login(WA_email, WA_password)
+    go_to_class("MC 106, section A, Fall 2020")
 
-soup = request(current_url)
-output(soup)
+    current_url = driver.current_url
+
+    soup = request(current_url)
+    output(soup)
