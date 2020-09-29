ba64749f4d060a003902d3e63197644e2911035d
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2020-09-29 15:13:05 -0600

    modified gspread functions

--- wa_worker/worker.py
+++ wa_worker/worker.py
@@ -10,17 +10,21 @@ def login(username, password):
 def go_to_class(class_name):
     driver.find_element_by_xpath('//*[@id="list"]/li[5]/a').click()
     driver.find_element_by_xpath('//*[@id="list"]/li[5]/ul/li[3]/a').click()
+
     select = Select(driver.find_element_by_id('scourse'))
     select.select_by_visible_text(class_name)
 
 
 def get_student_scores():
+
     table = driver.find_element_by_xpath(
         '//*[@id="table-wrapper"]/div[1]/div[2]/table')
+
     scores = []
     for row in table.find_elements_by_xpath(".//tr"):
         [scores.append(td.text) for td in row.find_elements_by_xpath(
-            '//*[@id="11475531"]/td[4]/font')]
+            '//*[@id="11588030"]/td[4]/font')]
+
     return scores
 
 
@@ -28,13 +32,18 @@ def parse(soup):
     pass
 
 
-def output(notas):
-    gc = gspread.service_account(filename='wa_worker/creds.json')
-    sh = gc.open('scrapetosheets').sheet1
-    sh.update('J1', int(notas[0]))
-    notas.pop(0)
-    for i in range(len(notas)):
-        sh.update(f'J{3 + i}', int(notas[i]))
+def load_gspread(creds_file_path, spread_name):
+    gc = gspread.service_account(filename=creds_file_path)
+    sh = gc.open(spread_name)
+    return sh
+
+
+def update_webassign(sh, sheet_name, scores):
+    worksheet = sh.worksheet(sheet_name)
+    worksheet.update('J1', int(scores[0]))
+    scores.pop(0)
+    for i in range(len(scores)):
+        worksheet.update(f'J{3 + i}', int(scores[i]))
 
 
 if __name__ == "__main__":
@@ -55,4 +64,8 @@ if __name__ == "__main__":
     go_to_class("MC 106, section A, Fall 2020")
 
     student_scores = get_student_scores()
-    output(student_scores)
+    print(student_scores)
+
+    sh = load_gspread('wa_worker/creds.json', 'scrapetosheets')
+    update_webassign(sh,
+                     'NotasFinales',  student_scores)
